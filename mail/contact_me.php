d6a9976abc47282fcc001e19cf5764c2792c3ad3
Muammar El Khatib <muammarelkhatib@gmail.com>
2014-10-11 11:02:34 +0200

    Contact form fixed

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -18,7 +18,7 @@ $message = $_POST['message'];
 // Create the email and send the message
 $to = 'lttc@irsamc.ups-tlse.fr'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Website Contact Form:  $name";
-$email_body = "You have received a new message from LTTC website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
+$email_body = "You have received a new message from LTTC website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\nSubject: $phone\n\nMessage:\n$message";
 $headers = "From: $email_address\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";
 mail($to,$email_subject,$email_body,$headers);
